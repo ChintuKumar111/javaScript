bee5bd836100d5a61910361a5c80042b5c109123
chintukumar111 <chintudas499@gmail.com>
2025-08-28 00:10:50 +0530

    amazon product details question for begginer

--- objectInJS.js
+++ objectInJS.js
@@ -10,6 +10,8 @@ Student.FullName = "Mikasa Ackerman"
 Student.age = Student.age+1
 console.log(Student);
 
-let name = "levi Ackermnan from Attack on titan";
+let name = "levi Ackermnan from Attack  titan";
+
+
 let NewName  = "cz star "
 console.log(name+"  "+ NewName);
